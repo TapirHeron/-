6538011c48429d44b502fc49d997b8eb98a0e734
08101813 <08101813@gitlab.com>
2025-04-14 19:36:40 +0800

    Changes

--- src/main/java/org/example/backend/service/DataService.java
+++ src/main/java/org/example/backend/service/DataService.java
@@ -4,6 +4,7 @@ import org.example.backend.pojo.Response.AnalysisResponse;
 import org.example.backend.pojo.UserTransData;
 import org.example.backend.repository.IDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -13,6 +14,10 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.FileSystem;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -21,22 +26,21 @@ public class DataService implements IDataService {
     private IDataRepository dataRepository;
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public AnalysisResponse saveAndAnalysis(UserTransData userTransData, MultipartFile image) throws Exception {
+    public AnalysisResponse saveAndAnalysis(UserTransData userTransData) throws Exception {
         dataRepository.save(userTransData);
-        return analysis(userTransData, image);
+        return analysis(userTransData);
     }
     // 调用大模型服务
-    private AnalysisResponse analysis(UserTransData userTransData, MultipartFile image) throws Exception{
+    private AnalysisResponse analysis(UserTransData userTransData) throws Exception{
         String text = userTransData.getText();
-        String url = "http://10.129.0.209:8081/upload";
-
+        String url = "http://localhost:8081/upload";
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         body.add("text", text);
-        body.add("image", image);
-
+        body.add("image", new FileSystemResource(userTransData.getImage()));
+        System.out.println("body:" + body);
         HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
 
         try {
@@ -51,6 +55,7 @@ public class DataService implements IDataService {
                 }
             }
         } catch (Exception e) {
+            e.printStackTrace();
             throw new Exception("上传失败", e);
         }
         return new AnalysisResponse(new JSONObject(), false);
